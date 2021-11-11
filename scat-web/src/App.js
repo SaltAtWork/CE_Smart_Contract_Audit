b855dd37d0053190755ddac26a696f8252d40e14
SaltAtWork <60347847+SaltAtWork@users.noreply.github.com>
2021-11-11 13:30:21 +0700

    Revert "Test Base Component"
    This reverts commit 7125c4f5b45b0364be796095beb2bd1a64e69d5d.

--- scat-web/src/App.js
+++ scat-web/src/App.js
@@ -1,79 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import TierListItem from './components/TierListItem.js';
-import {v4 as uuidv4} from 'uuid';
-
-const Title =()=><h1 id="title">[Insert Project Name Here]</h1>
-
-const FilterList = [
-  "All",
-  "Lending",
-  "DEXes"
-]
-
-const TierListItemList = [
-  {
-    ranking:"Rank",
-    defiName:"Name",
-    chainType:"Chain",
-    categoryType:"Category",
-    totalValueLocked:"TVL",
-    oneDayChange:"1 Day%"
-  },
-  {
-    ranking:"1",
-    defiName:"Aave",
-    chainType:"Multichain",
-    categoryType:"Lending",
-    totalValueLocked:"$15.51B",
-    oneDayChange:"2.58%"
-  },
-  {
-    ranking:"2",
-    defiName:"Curve Finance",
-    chainType:"Multichain",
-    categoryType:"DEXes",
-    totalValueLocked:"$13.44B",
-    oneDayChange:"10.41%"
-  },
-]
-
-const Filter =()=>{
-  return(
-    <ul>
-      {FilterList.map((element)=>{
-        return <li> {element} </li>
-      })}
-    </ul>
-  );
-}
-
-const TierList =()=>{
-  return(
-    <div class="col-9">
-      {TierListItemList.map((element)=>{
-        return <TierListItem input={element} />
-      })}
-    </div>
-  );
-}
-
-const DeFiList =()=>{
-  return(
-    <div class="row"> 
-        <div class="col-3 filter">
-          <span>Filter</span>
-          <Filter />
-        </div>
-        <TierList />
-      </div>
-  );
-}
 
 function App() {
   return (
-    <div>
-      <Title />
-      <DeFiList />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
