b9e1a94122e4c25c294dea0c2d1417b93f0a1e86
SaltAtWork <60347847+SaltAtWork@users.noreply.github.com>
2022-04-23 20:39:22 +0700

    Fixed Possibly False Positive in IncorrectOP - 4/23/2022

--- smart_contract_scanner/pattern/test_IncorrectOP.js
+++ smart_contract_scanner/pattern/test_IncorrectOP.js
@@ -5,11 +5,14 @@ function testIncorrectOP(ast) {
     try {
         parser.visit(ast, {
             BinaryOperation: (node) => {
-                if (node.right.type == "UnaryOperation") {
+                if (node.right.type == "UnaryOperation" && node.right.operator != "!") {
                     callIndex = node.range
                     console.log('\nDetected Operator typo\n')
                     console.log('position ' + callIndex + '\n')
                     detect = detect + '\nDetected Operator typo \n' + 'position ' + callIndex + '\n'
+                    
+                    //For Debugging
+                    detect = detect + node.operator + '\n'
                 }
             }
         })
