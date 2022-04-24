be998479ddcc28b476492930d368f4c979ed9b8c
reheter <51040146+reheter@users.noreply.github.com>
2022-04-24 16:01:13 +0700

    audited
    audit result of 40 defi project

--- smart_contract_scanner/Salt_AuditCase/FinyTron/FinyTron.sol
+++ smart_contract_scanner/Salt_AuditCase/FinyTron/FinyTron.sol
@@ -1,98 +1,26 @@
-/**
- *Submitted for verification at polygonscan.com on 2021-09-25
-*/
+pragma solidity 0.5.12;
 
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.6.12;
-
-/*
- * @dev Provides information about the current execution context, including the
- * sender of the transaction and its data. While these are generally available
- * via msg.sender and msg.data, they should not be accessed in such a direct
- * manner, since when dealing with meta-transactions the account sending and
- * paying for execution may not be the actual sender (as far as an application
- * is concerned).
- *
- * This contract is only required for intermediate, library-like contracts.
- */
-abstract contract Context {
-    function _msgSender() internal view virtual returns (address) {
-        return msg.sender;
-    }
-
-    function _msgData() internal view virtual returns (bytes memory) {
-        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
-        return msg.data;
-    }
-}
-
-pragma solidity ^0.6.12;
-
-contract Ownable is Context {
-    address private _owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    /**
-     * @dev Initializes the contract setting the deployer as the initial owner.
-     */
-    constructor () public{
-        address msgSender = _msgSender();
-        _owner = msgSender;
-        emit OwnershipTransferred(address(0), msgSender);
-    }
-
-    /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view returns (address) {
-        return _owner;
-    }
-    
-
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        require(_owner == _msgSender(), "Ownable: caller is not the owner");
-        _;
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     */
-    function transferOwnership(address newOwner) public virtual onlyOwner {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        emit OwnershipTransferred(_owner, newOwner);
-        _owner = newOwner;
-    }
-}
-
-pragma solidity ^0.6.12;
-
-contract MATICX2 is Ownable{
+contract FinyTron {
 	using SafeMath for uint256;
 
-	uint256 constant public INVEST_MIN_AMOUNT = 10 ether;
-	uint256 constant public BASE_PERCENT = 50;
-	uint256[] public REFERRAL_PERCENTS = [50, 20, 10];
-	uint256 constant public MARKETING_FEE = 40;
-	uint256 constant public PROJECT_FEE = 40;
-	uint256 constant public FUND_FEE=40;
+	uint256 constant public INVEST_MIN_AMOUNT = 200 ;//trx
+	uint256 constant public BASE_PERCENT = 15;
+	uint256[] public REFERRAL_PERCENTS = [50, 30, 10];
+	uint256 constant public MARKETING_FEE = 70;
+	uint256 constant public RESERVE_FEE = 15;
+	uint256 constant public PROJECT_FEE = 15;
 	uint256 constant public PERCENTS_DIVIDER = 1000;
-	uint256 constant public TIME_STEP = 1 days; //1 days
+	uint256 constant public CONTRACT_BALANCE_STEP = 750000 ;//trx
+	uint256 constant public TIME_STEP = 1 days;
 
 	uint256 public totalUsers;
 	uint256 public totalInvested;
 	uint256 public totalWithdrawn;
 	uint256 public totalDeposits;
-	uint256 public startUNIX;
 
 	address payable public marketingAddress;
-	address payable public fundAddress;
 	address payable public projectAddress;
+	address payable public reserveAddress;
 
 	struct Deposit {
 		uint256 amount;
@@ -105,7 +33,6 @@ contract MATICX2 is Ownable{
 		uint256 checkpoint;
 		address referrer;
 		uint256 bonus;
-		uint256 refs;
 	}
 
 	mapping (address => User) internal users;
@@ -116,60 +43,39 @@ contract MATICX2 is Ownable{
 	event RefBonus(address indexed referrer, address indexed referral, uint256 indexed level, uint256 amount);
 	event FeePayed(address indexed user, uint256 totalAmount);
 
-	constructor(address payable marketingAddr, address payable projectAddr, address payable fundAddr, uint256 startDate) public {
-		require(!isContract(marketingAddr) && !isContract(projectAddr) && !isContract(fundAddr));
+	constructor(address payable marketingAddr, address payable projectAddr, address payable reserveAddr) public {
+		require(!isContract(marketingAddr) && !isContract(projectAddr) && !isContract(reserveAddr));
 		marketingAddress = marketingAddr;
 		projectAddress = projectAddr;
-		fundAddress = fundAddr;
-		startUNIX = startDate;
+		reserveAddress = reserveAddr;
 	}
-	
-	function FeePayout(uint256 msgValue) internal{
-    marketingAddress.transfer(msgValue.mul(MARKETING_FEE).div(PERCENTS_DIVIDER));
-    projectAddress.transfer(msgValue.mul(PROJECT_FEE).div(PERCENTS_DIVIDER));
-    fundAddress.transfer(msgValue.mul(FUND_FEE).div(PERCENTS_DIVIDER));
-    emit FeePayed(msg.sender,msgValue.mul(MARKETING_FEE.add(PROJECT_FEE).add(FUND_FEE)).div(PERCENTS_DIVIDER));
-    }
 
 	function invest(address referrer) public payable {
-	    require(block.timestamp >= startUNIX ,"Not Launch");
 		require(msg.value >= INVEST_MIN_AMOUNT);
-	    FeePayout(msg.value);
+
+		marketingAddress.transfer(msg.value.mul(MARKETING_FEE).div(PERCENTS_DIVIDER));
+		projectAddress.transfer(msg.value.mul(PROJECT_FEE).div(PERCENTS_DIVIDER));
+		reserveAddress.transfer(msg.value.mul(RESERVE_FEE).div(PERCENTS_DIVIDER));
+		emit FeePayed(msg.sender, msg.value.mul(MARKETING_FEE.add(PROJECT_FEE).add(RESERVE_FEE)).div(PERCENTS_DIVIDER));
 
 		User storage user = users[msg.sender];
-		
-		if (user.referrer == address(0)) {
-			if (users[referrer].deposits.length > 0 && referrer != msg.sender) {
-				user.referrer = referrer;
-			}
-		    
+
+		if (user.referrer == address(0) && users[referrer].deposits.length > 0 && referrer != msg.sender) {
+			user.referrer = referrer;
 		}
-		uint256 refsamount;
-         if(user.referrer != address(0)){
+
+		if (user.referrer != address(0)) {
+
 			address upline = user.referrer;
 			for (uint256 i = 0; i < 3; i++) {
 				if (upline != address(0)) {
 					uint256 amount = msg.value.mul(REFERRAL_PERCENTS[i]).div(PERCENTS_DIVIDER);
 					users[upline].bonus = users[upline].bonus.add(amount);
-					users[upline].refs = users[upline].refs.add(1);
 					emit RefBonus(upline, msg.sender, i, amount);
 					upline = users[upline].referrer;
-				} else{
-				    uint256 amount = msg.value.mul(REFERRAL_PERCENTS[i]).div(PERCENTS_DIVIDER);
-				    refsamount = refsamount.add(amount);
-				}
+				} else break;
 			}
-			if (refsamount > 0){
-			marketingAddress.transfer(refsamount.div(3));
-            projectAddress.transfer(refsamount.div(3));
-            fundAddress.transfer(refsamount.div(3));
-			}
-         }else{
-		    uint256 refsbkp = 80;
-		    uint256 amount = msg.value.mul(refsbkp).div(PERCENTS_DIVIDER);
-		    marketingAddress.transfer(amount.div(3));
-            projectAddress.transfer(amount.div(3));
-            fundAddress.transfer(amount.div(3));
+
 		}
 
 		if (user.deposits.length == 0) {
@@ -190,29 +96,31 @@ contract MATICX2 is Ownable{
 	function withdraw() public {
 		User storage user = users[msg.sender];
 
+		uint256 userPercentRate = getUserPercentRate(msg.sender);
+
 		uint256 totalAmount;
 		uint256 dividends;
 
 		for (uint256 i = 0; i < user.deposits.length; i++) {
 
-			if (user.deposits[i].withdrawn < user.deposits[i].amount.mul(2)) {
+			if (user.deposits[i].withdrawn < user.deposits[i].amount.mul(3)) {
 
 				if (user.deposits[i].start > user.checkpoint) {
 
-					dividends = (user.deposits[i].amount.mul(BASE_PERCENT).div(PERCENTS_DIVIDER))
+					dividends = (user.deposits[i].amount.mul(userPercentRate).div(PERCENTS_DIVIDER))
 						.mul(block.timestamp.sub(user.deposits[i].start))
 						.div(TIME_STEP);
 
 				} else {
 
-					dividends = (user.deposits[i].amount.mul(BASE_PERCENT).div(PERCENTS_DIVIDER))
+					dividends = (user.deposits[i].amount.mul(userPercentRate).div(PERCENTS_DIVIDER))
 						.mul(block.timestamp.sub(user.checkpoint))
 						.div(TIME_STEP);
 
 				}
 
-				if (user.deposits[i].withdrawn.add(dividends) > user.deposits[i].amount.mul(2)) {
-					dividends = (user.deposits[i].amount.mul(2)).sub(user.deposits[i].withdrawn);
+				if (user.deposits[i].withdrawn.add(dividends) > user.deposits[i].amount.mul(3)) {
+					dividends = (user.deposits[i].amount.mul(3)).sub(user.deposits[i].withdrawn);
 				}
 
 				user.deposits[i].withdrawn = user.deposits[i].withdrawn.add(dividends); /// changing of storage data
@@ -243,58 +151,57 @@ contract MATICX2 is Ownable{
 		emit Withdrawn(msg.sender, totalAmount);
 
 	}
-	
-	function updateStartUNIX(uint256 unixtime) public onlyOwner{
-	    startUNIX = unixtime;
-	    
-	}
-	
-	function updateFundAddress(address payable newAddress) public onlyOwner{
-	    fundAddress = newAddress;
-	    
-	}
-	
-	function updateMarketingAddress(address payable newAddress) public onlyOwner{
-	    marketingAddress = newAddress;
-	    
-	}
-	
-	function updateProjectAddress(address payable newAddress) public onlyOwner{
-	    projectAddress = newAddress;
-	    
-	}
 
 	function getContractBalance() public view returns (uint256) {
 		return address(this).balance;
 	}
-	
+
+	function getContractBalanceRate() public view returns (uint256) {
+		uint256 contractBalance = address(this).balance;
+		uint256 contractBalancePercent = contractBalance.div(CONTRACT_BALANCE_STEP);
+		return BASE_PERCENT.add(contractBalancePercent);
+	}
+
+	function getUserPercentRate(address userAddress) public view returns (uint256) {
+		User storage user = users[userAddress];
+
+		uint256 contractBalanceRate = getContractBalanceRate();
+		if (isActive(userAddress)) {
+			uint256 timeMultiplier = (now.sub(user.checkpoint)).div(TIME_STEP);
+			return contractBalanceRate.add(timeMultiplier);
+		} else {
+			return contractBalanceRate;
+		}
+	}
 
 	function getUserDividends(address userAddress) public view returns (uint256) {
 		User storage user = users[userAddress];
+
+		uint256 userPercentRate = getUserPercentRate(userAddress);
 
 		uint256 totalDividends;
 		uint256 dividends;
 
 		for (uint256 i = 0; i < user.deposits.length; i++) {
 
-			if (user.deposits[i].withdrawn < user.deposits[i].amount.mul(2)) {
+			if (user.deposits[i].withdrawn < user.deposits[i].amount.mul(3)) {
 
 				if (user.deposits[i].start > user.checkpoint) {
 
-					dividends = (user.deposits[i].amount.mul(BASE_PERCENT).div(PERCENTS_DIVIDER))
+					dividends = (user.deposits[i].amount.mul(userPercentRate).div(PERCENTS_DIVIDER))
 						.mul(block.timestamp.sub(user.deposits[i].start))
 						.div(TIME_STEP);
 
 				} else {
 
-					dividends = (user.deposits[i].amount.mul(BASE_PERCENT).div(PERCENTS_DIVIDER))
+					dividends = (user.deposits[i].amount.mul(userPercentRate).div(PERCENTS_DIVIDER))
 						.mul(block.timestamp.sub(user.checkpoint))
 						.div(TIME_STEP);
 
 				}
 
-				if (user.deposits[i].withdrawn.add(dividends) > user.deposits[i].amount.mul(2)) {
-					dividends = (user.deposits[i].amount.mul(2)).sub(user.deposits[i].withdrawn);
+				if (user.deposits[i].withdrawn.add(dividends) > user.deposits[i].amount.mul(3)) {
+					dividends = (user.deposits[i].amount.mul(3)).sub(user.deposits[i].withdrawn);
 				}
 
 				totalDividends = totalDividends.add(dividends);
@@ -319,10 +226,6 @@ contract MATICX2 is Ownable{
 	function getUserReferralBonus(address userAddress) public view returns(uint256) {
 		return users[userAddress].bonus;
 	}
-	
-	function getUserReferralRefs(address userAddress) public view returns(uint256) {
-		return users[userAddress].refs;
-	}
 
 	function getUserAvailable(address userAddress) public view returns(uint256) {
 		return getUserReferralBonus(userAddress).add(getUserDividends(userAddress));
@@ -332,7 +235,7 @@ contract MATICX2 is Ownable{
 		User storage user = users[userAddress];
 
 		if (user.deposits.length > 0) {
-			if (user.deposits[user.deposits.length-1].withdrawn < user.deposits[user.deposits.length-1].amount.mul(2)) {
+			if (user.deposits[user.deposits.length-1].withdrawn < user.deposits[user.deposits.length-1].amount.mul(3)) {
 				return true;
 			}
 		}
@@ -371,7 +274,6 @@ contract MATICX2 is Ownable{
 
 		return amount;
 	}
-	
 
 	function isContract(address addr) internal view returns (bool) {
         uint size;
